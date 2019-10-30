02ae793a99303144dc5c1d8170db7ff5c6b30308
Aleksandr Nester <Aleksandrnes08@mail.ru>
2019-10-30 13:53:40 +0300

    rename variables

--- frameworkforhardcoretask/src/test/java/model/CalculatorData.java
+++ frameworkforhardcoretask/src/test/java/model/CalculatorData.java
@@ -2,59 +2,59 @@ package model;
 
 public class CalculatorData {
 
-    private String ValueOfOperatingSystem;
-    private String ValueOfMachineClass;
-    private String ValueOfMachineType;
-    private String ValueOfNumberOfGPUs;
-    private String ValueOfGPUType;
-    private String ValueOfLocalSSD;
-    private String ValueOfDatacenterLocation;
-    private String ValueOfCommitedUsage;
+    private String valueOfOperatingSystem;
+    private String valueOfMachineClass;
+    private String valueOfMachineType;
+    private String valueOfNumberOfGPUs;
+    private String valueOfGPUType;
+    private String valueOfLocalSSD;
+    private String valueOfDatacenterLocation;
+    private String valueOfCommitedUsage;
 
 
     public CalculatorData(String valueOfOperatingSystem, String valueOfMachineClass, String valueOfMachineType,
                           String valueOfNumberOfGPUs, String valueOfGPUType, String valueOfLocalSSD,
                           String valueOfDatacenterLocation, String valueOfCommitedUsage){
-        ValueOfOperatingSystem = valueOfOperatingSystem;
-        ValueOfMachineClass = valueOfMachineClass;
-        ValueOfMachineType = valueOfMachineType;
-        ValueOfNumberOfGPUs = valueOfNumberOfGPUs;
-        ValueOfGPUType = valueOfGPUType;
-        ValueOfLocalSSD = valueOfLocalSSD;
-        ValueOfDatacenterLocation = valueOfDatacenterLocation;
-        ValueOfCommitedUsage = valueOfCommitedUsage;
+        this.valueOfOperatingSystem = valueOfOperatingSystem;
+        this.valueOfMachineClass = valueOfMachineClass;
+        this.valueOfMachineType = valueOfMachineType;
+        this.valueOfNumberOfGPUs = valueOfNumberOfGPUs;
+        this.valueOfGPUType = valueOfGPUType;
+        this.valueOfLocalSSD = valueOfLocalSSD;
+        this.valueOfDatacenterLocation = valueOfDatacenterLocation;
+        this.valueOfCommitedUsage = valueOfCommitedUsage;
     }
 
     public String getValueOfOperatingSystem() {
-        return ValueOfOperatingSystem;
+        return valueOfOperatingSystem;
     }
 
     public String getValueOfMachineClass() {
-        return ValueOfMachineClass;
+        return valueOfMachineClass;
     }
 
     public String getValueOfMachineType() {
-        return ValueOfMachineType;
+        return valueOfMachineType;
     }
 
     public String getValueOfNumberOfGPUs() {
-        return ValueOfNumberOfGPUs;
+        return valueOfNumberOfGPUs;
     }
 
     public String getValueOfGPUType() {
-        return ValueOfGPUType;
+        return valueOfGPUType;
     }
 
     public String getValueOfLocalSSD() {
-        return ValueOfLocalSSD;
+        return valueOfLocalSSD;
     }
 
     public String getValueOfDatacenterLocation() {
-        return ValueOfDatacenterLocation;
+        return valueOfDatacenterLocation;
     }
 
     public String getValueOfCommitedUsage() {
-        return ValueOfCommitedUsage;
+        return valueOfCommitedUsage;
     }
 
 
